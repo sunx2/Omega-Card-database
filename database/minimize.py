837920a23adbe757b387cfbb60fd10e5e0a893cd
sun <xxsunxxrose@gmail.com>
2021-07-13 20:41:44 +0530

    added mimisiser

---
+++ database/minimize.py
@@ -0,0 +1,20 @@
+import json
+
+data = json.load(open("database/data.json"))
+banlist = json.load(open('database/banlist.json'))
+cards = list(data.keys())
+
+
+
+
+for language in ["en","de","fr","es","it","ko","pt","ja"]:
+    languagex = {}
+    for i in cards:
+        languagex[i] = data[i]
+        del languagex[i]['text']
+        del languagex[i]['images']
+        languagex[i]['n'] = data[i]['text'][language]['name']
+        language[i]["d"] = data[i]['text'][language]['name']
+    json.dump(languagex,open(f"/database/lang/data-{language}-min.json"),separators=(',',':'))
+
+
