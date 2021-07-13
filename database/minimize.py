7252cf727e34ddc07fa110ece65700b1c399d244
sun <xxsunxxrose@gmail.com>
2021-07-13 21:06:12 +0530

    fixed minimize

--- database/minimize.py
+++ database/minimize.py
@@ -5,16 +5,33 @@ banlist = json.load(open('database/banlist.json'))
 cards = list(data.keys())
 
 
-
+massive = {}
 
 for language in ["en","de","fr","es","it","ko","pt","ja"]:
     languagex = {}
     for i in cards:
-        languagex[i] = data[i]
-        languagex[i]['n'] = data[i]['text'][language]['name']
-        languagex[i]["d"] = data[i]['text'][language]['name']
-        del languagex[i]['text']
-        del languagex[i]['images']
-    json.dump(languagex,open(f"database/lang/data-{language}-min.json","w"),separators=(',',':'))
+        try:
+            languagex[i] = data[i]
+            try:
+                languagex[i]['n'] = data[i]['text'][language]['name']
+                languagex[i]["d"] = data[i]['text'][language]['desc']
+            except:
+                    languagex[i]['n'] = data[i]['text']["en"]['name']
+                    languagex[i]["d"] = data[i]['text']["en"]['desc']
+                    languagex['notrans'] = True
+        except Exception as r:
+            print(r,data[i]['text'].keys())
+            break
+    massive[language] = languagex
+
+
+for language in ["en","de","fr","es","it","ko","pt","ja"]:
+    for i in cards:
+        try:
+            del massive[language][i]['text']
+            del massive[language][i]['images']
+        except:
+            break
+    json.dump(massive[language],open(f"database/lang/data-{language}-min.json","w"),separators=(',',':'))
 
 
